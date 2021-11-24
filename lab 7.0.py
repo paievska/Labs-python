766aec19bb71771fa7b258e38e75d9ab6de6b035
Paievska <dianapaievska111@gmail.com>
2021-11-24 19:54:16 +0200

    commit

--- lab 7.0.py
+++ lab 7.0.py
@@ -2,35 +2,6 @@ from numpy import*
 import math
 import matplotlib.pyplot as plt 
  
-plt.plot([1, 2, 3, 4]) 
-plt.show() 
- 
-def f(t): 
-    return t **2 * math.exp(-t**2) 
-t = linspace(0, 3, 51) 
-y = f(t) 
-plt.plot(t, y) 
-plt.show() 
- 
-t = linspace(0, 3, 51) 
-plt.plot(t, y, 'g--', label = 't^2*exp(-t^2)') 
-plt.axis([0, 3, -0.05, 0.5])  
-plt.xlabel('t') 
-plt.xlabel('y') 
-plt.title('My first normal plot') 
-plt.legend() 
-plt.show() 
- 
-y1 = t**2 * math.exp(-t**2) 
-y2 = t**4 * math.exp(-t**2) 
-y3 = t**6 * math.exp(-t**2) 
-plt.plot(t, y1, 'g^', t, y2, 'b--', t, y3, 'ro-') 
-plt.xlabel('t') 
-plt.ylabel('y') 
-plt.title('Plotting with markers') 
-plt.legend(['t^2*exp(-t^2)','t^4*exp(-t^2)', 't^6*exp(-t^2)'], loc = 'upper left') 
-plt.show() 
- 
 def f1(t): 
     return 10 * (math.cos(t**2)/(t**2) )
 t = linspace(0, 4) 
@@ -42,6 +13,6 @@ plt.axis([0, 4, -5, 5])
 plt.grid() 
 plt.xlabel('t') 
 plt.xlabel('y') 
-plt.title('5 * sin(10 * x) * sin(3 * x)') 
+plt.title('10*cos(x^2)/x^2') 
 plt.legend() 
 plt.show()
