41d7ad914da40565c12c3a5ef91e0ef8ff66e8bb
Paievska <dianapaievska111@gmail.com>
2021-11-24 20:09:08 +0200

    commit

--- lab 7.0.py
+++ lab 7.0.py
@@ -1,18 +1,10 @@
-from numpy import* 
+from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
 import math
-import matplotlib.pyplot as plt 
- 
-def f1(t): 
-    return 10 * (math.cos(t**2)/(t**2) )
-t = linspace(0, 4) 
-y = f1(t) 
- 
-t = linspace(0, 4) 
-plt.plot(t, y, 'g-', label = '10*cos(x^2)/x^2', color = "pink") 
-plt.axis([0, 4, -5, 5])  
-plt.grid() 
-plt.xlabel('t') 
-plt.xlabel('y') 
-plt.title('10*cos(x^2)/x^2') 
-plt.legend() 
+t = linspace(0, 4, 51)
+y = 10*np.cos(t*t) / (t*t)
+plt.plot(t, y, 'g--', label='10*cos(x^2)/(x^2)')
+plt.axis([0, 4, -5, 10]) 
+plt.legend() # вставка легенди (тексту в label)
 plt.show()
